14e528250b5eccbf947c7c0b712791cecdb7cabd
unknown <victormagallanes2@gmail.com>
2026-08-09 16:36:30 -0400

    se elimino almacen y invoicing definitivamente

--- django_erp/invoicing/module_info.py
+++
@@ -1,8 +0,0 @@
-# invoicing/module_info.py
-MODULE_INFO = {
-    'name': 'invoicing',
-    'verbose_name': 'Facturación',
-    'description': 'Gestión de facturación',
-    'version': '1.0.0',
-    'dependencies': ['sales', 'warehouse'],  # ← Requiere Sales
-}
